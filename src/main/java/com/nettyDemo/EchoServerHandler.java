b6267c5e20d7858f1bda88eacc449d273625f5c5
t777 <Luo2017a@163.com>
2021-11-19 16:34:25 +0800

    the first handler

---
+++ src/main/java/com/nettyDemo/EchoServerHandler.java
@@ -0,0 +1,31 @@
+package com.nettyDemo;
+
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("服务器收到消息：" + in.toString(CharsetUtil.UTF_8));
+        ctx.write(in); // 只是写，不 flush
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace(); // 不在此处拦截，异常信息会传输到 pipeline 的尾端
+        ctx.close(); // 关闭该 ctx
+    }
+}
---
+++ src/main/java/com/nettyDemo/NettyServer.java
@@ -0,0 +1,5 @@
+package com.nettyDemo;
+
+public class NettyServer {
+
+}
