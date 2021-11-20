601cb9b3daabebc2a61e6970f612e0cbc4b9e4ee
t777 <Luo2017a@163.com>
2021-11-20 15:44:46 +0800

    cv

---
+++ src/main/java/com/nettyExperiment/EchoClientHandler.java
@@ -0,0 +1,29 @@
+package com.nettyExperiment;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+@ChannelHandler.Sharable
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        // 当被通知 channel 活跃时，发送一条消息，即与服务器的链接建立之后被调用
+        System.out.println("客户端 channel active!");
+        ctx.writeAndFlush(Unpooled.copiedBuffer("客户端发来的消息-客户端已经 active", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+        System.out.println("客户端接收到服务端发送的消息: " + byteBuf.toString(CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close(); // 关闭 channel，返回 ChannelFuture
+    }
+}
---
+++ src/main/java/com/nettyExperiment/NettyClient.java
@@ -0,0 +1,48 @@
+package com.nettyExperiment;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
+public class NettyClient {
+
+    private final String host;
+
+    private final int port;
+
+    public NettyClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class) // 注意和服务端 NioServerSocketChannel 区别
+                    .remoteAddress(new InetSocketAddress(host, port)) // java io 包中 host 就是 String 类型的
+                    .handler(new ChannelInitializer<SocketChannel>() { // 注意服务器端的是 childHandler
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+            ChannelFuture f = b.connect().sync(); // 注意不是 bind() 监听端口
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new NettyClient("127.0.0.1", 8088).start();
+    }
+
+}
